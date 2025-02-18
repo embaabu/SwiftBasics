1143a75a2e10f608310bd6bffdbeeef81ba7a15d
embaabu <edwin.mbaabu@techconsulting.tech>
2025-02-18 09:19:19 -0500

    Practice on Swift Basics on playGround

---
+++ Example1.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+//using chaining to unwrap the optional
+class Person{
+    var name: String
+    var age: Int
+    var car: [String]?
+    init(name: String, age: Int, car: [String]?) {
+        self.name = name
+        self.age = age
+        self.car = car
+    }
+    func increment (){
+        self.age += 1
+    }
+    
+}
+
+let p1 = Person(name: "Edwin", age: 12, car: ["BMW" ,"Honda", "VW"]
+)
+//p1.car = "BMW"
+p1.increment()
+print(p1.age)
+// we can use optional binding to condtitionally bind the wrapped value
+//if let unwrappedString = p1.car {
+//    print(unwrappedString)
+//    
+//}else{
+//    print("Optional is nil")
+//}
+//print(p1.car)
+// we can try and use optional chaining
+
+if let firstCar = p1.car?.first{
+    print("\(p1.name) drives \(firstCar)")
+}
+
